3fed7877774954136203b09dd674d49e6ba85a27
Ryan Archer <ryan.bradley.archer@gmail.com>
2015-04-18 19:30:32 -0400

    Fix the Linux start script not working in builds made on Windows.
    Installation paths are currently stored in a platform-neutral way; when
    compiled on Windows, they render with Windows conventions, breaking the
    Linux start script. This makes the build process less flexible and risks
    human error, so make the paths in question platform-specific to avoid the
    problem.

--- Server/src.config/start-script.sh
+++ Server/src.config/start-script.sh
@@ -4,4 +4,4 @@
 # 
 # This file is sourced by Xsession(5), not executed.
 
-/usr/bin/java -jar @install.linux.dir@/@jar.path.relative@ &
+/usr/bin/java -jar @install.linux.dir@/@jar.path.relative.linux@ &
